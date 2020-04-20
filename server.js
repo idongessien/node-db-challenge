4295e3848ec5f4e25f32163e8520a686fe099d8b
Idong Essien <idong.essien@comcast.net>
2020-04-20 03:26:31 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
 const server = express();
+const projectRouter = require('../routes/projectRoutes');
 
-server.get('/', (req, res) => {
-    res.send('Hello from Express');
+server.use(express.json());
+server.use('/api/projects', projectRouter)
+server.get("/", (req, res) => {
+    res.send("<h2>Def Worst Than Redux !</h2>");
 });
 
-server.listen(5000, () =>
-    console.log('=== Server running on http://localhost:5000 ===')
-);
+module.exports = server;
